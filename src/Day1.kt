fa23cd8d82085baed65d002cd81e17bbf2c29f19
theboxmage <joshua.h.graham96@gmail.com>
2021-12-06 15:19:11 -0500

    Fixes Day 1 IDE recommendations

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,9 +1,9 @@
 class Day1(fileName: String) {
-    val fileService = FileService(fileName)
+    private val fileService = FileService(fileName)
+    private val lines = fileService.getLinesAsInt()
 
     fun solvePart1(): String {
         var count = 0
-        val lines = fileService.getLinesAsInt()
         for (i in 0..lines.size - 2) {
             count += if (lines[i] < lines[i + 1]) 1 else 0
         }
@@ -12,7 +12,6 @@ class Day1(fileName: String) {
 
     fun solvePart2(): String {
         var count = 0
-        val lines = fileService.getLinesAsInt()
         for (i in 0..lines.size-4) {
             val num1 = lines[i] + lines[i+1] + lines[i+2]
             val num2 = lines[i+1] + lines[i+2] + lines[i+3]
