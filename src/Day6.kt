9eec634da82896409aa5d11b69c4586456f7dc51
theboxmage <joshua.h.graham96@gmail.com>
2021-12-06 15:13:26 -0500

    Removing streams, I'm using them where I don't have to

--- src/Day6.kt
+++ src/Day6.kt
@@ -1,5 +1,3 @@
-import java.util.stream.Collectors
-
 class Day6(filePath: String) {
     private val fileService = FileService(filePath)
     private val input = fileService.getLinesAsString()[0].split(",").map { it.toInt() }
@@ -10,14 +8,14 @@ class Day6(filePath: String) {
         for(i in 0 until 80) {
             runDay(fish)
         }
-        return fish.stream().collect(Collectors.summingLong { it })!!
+        return fish.sum()
     }
 
     fun solvePart2(): Long {
         for(i in 0 until 176) {
             runDay(fish)
         }
-        return fish.stream().collect(Collectors.summingLong { it })!!
+        return fish.sum()
     }
 
     private fun runDay(fish: ArrayList<Long>) {
