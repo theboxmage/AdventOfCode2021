d678bf45b6d207afc6073bdda240ebf5e6599f1c
theboxmage <joshua.h.graham96@gmail.com>
2021-12-06 23:31:25 -0500

    Creates Betterer loops

--- src/Day5.kt
+++ src/Day5.kt
@@ -1,51 +1,39 @@
 import java.util.stream.Collectors
+import kotlin.math.abs
+import kotlin.math.max
 
 class Day5(filePath: String) {
     private val fileService = FileService(filePath)
-    private val input = fileService.getLinesAsString() as ArrayList
+    private var input = fileService.getLinesAsString() as ArrayList
     private val map = LinkedHashMap<String, Int>()
 
-    fun solvePart1(): Long {
+    fun solvePart1(): Int {
         for (s in input.size - 1 downTo 0) {
             val p = Path(input[s])
-            if (p.x1 == p.x2) {
-                input.remove(input[s])
-                if (p.y1 < p.y2) {
-                    for (i in p.y1..p.y2) {
-                        addOrIncrement("${p.x1} $i")
-                    }
-                } else {
-                    for (i in p.y1 downTo p.y2) {
-                        addOrIncrement("${p.x1} $i")
-                    }
-                }
-            } else if (p.y1 == p.y2) {
-                input.remove(input[s])
-                if (p.x1 < p.x2) {
-                    for (i in p.x1..p.x2) {
-                        addOrIncrement("$i ${p.y1}")
-                    }
-                } else {
-                    for (i in p.x1 downTo p.x2) {
-                        addOrIncrement("$i ${p.y1}")
-                    }
+            if (p.x1 == p.x2 || p.y1 == p.y2) {
+                input.removeAt(s)
+                val distance = max(abs(p.y1 - p.y2), abs(p.x1 - p.x2))
+                for (i in 0..distance) {
+                    val x = if (p.x1 != p.x2) p.x1 + i * if (p.x1 < p.x2) 1 else -1 else p.x1
+                    val y = if (p.y1 != p.y2) p.y1 + i * if (p.y1 < p.y2) 1 else -1 else p.y1
+                    addOrIncrement("$x $y")
                 }
             }
         }
-        return map.toList().stream().filter { it.second > 1 }.count()
+        return map.filter{it.value > 1}.count()
     }
 
-    fun solvePart2() {
+    fun solvePart2(): Int {
         for (path in input) {
             val p = Path(path)
-            val distance = Math.abs(p.x1 - p.x2)
+            val distance = abs(p.x1 - p.x2)
             for (i in 0..distance) {
                 val x = p.x1 + (i * if (p.x1 < p.x2) 1 else -1)
                 val y = p.y1 + (i * if (p.y1 < p.y2) 1 else -1)
                 addOrIncrement("$x $y")
             }
         }
-        println(map.toList().stream().filter { it.second > 1 }.count())
+        return map.filter { it.value > 1 }.count()
     }
 
     private fun addOrIncrement(key: String) {
--- src/main.kt
+++ src/main.kt
@@ -1,37 +1,36 @@
 fun main() {
-    val line = "6"
-    when(line){
-        "1" -> {
+    when(5){
+        1 -> {
             val day1 = Day1("data/day1.txt")
             println("Day 1 Output")
-            println("Part 1: \n${day1.solvePart1()}")
-            println("Part 2: \n${day1.solvePart2()}")
+            println("Part 1: \n\t${day1.solvePart1()}")
+            println("Part 2: \n\t${day1.solvePart2()}")
         }
-        "2" -> {
+        2 -> {
             val day2 = Day2("data/day2.txt")
             println("Day 2 Output")
-            println("Part 1: \n${day2.solvePart1()}")
-            println("Part 2: \n${day2.solvePart2()}")
+            println("Part 1: \n\t${day2.solvePart1()}")
+            println("Part 2: \n\t${day2.solvePart2()}")
         }
-        "3" -> {
+        3 -> {
             val day3 = Day3("data/day3.txt")
             println("Day 3 Output")
             println("Part 1: \n\t${day3.solvePart1()}")
             println("Part 2: \n\t${day3.solvePart2()}")
         }
-        "4" -> {
+        4 -> {
             val day4 = Day4("data/day4.txt")
             println("Day 4 Output")
             println("Part 1: \n\t${day4.solvePart1()}")
             println("Part 2: \n\t${day4.solvePart2()}")
         }
-        "5" -> {
+        5 -> {
             val day5 = Day5("data/day5.txt")
             println("Day 5 Output")
-            println(day5.solvePart1())
-            day5.solvePart2()
+            println("Part 1: \n\t${day5.solvePart1()}")
+            println("Part 2: \n\t${day5.solvePart2()}")
         }
-        "6" -> {
+        6 -> {
             val day6 = Day6("data/day6.txt")
             println("Day 6 Output")
             println("Part 1: \n\t${day6.solvePart1()}")
