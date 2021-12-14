42cccb6527f78fd293c9db4cb1fab7ea75cf109d
TheBoxMage <!1a2s3dQWER>
2021-12-13 21:54:26 -0500

    Added ability to time parts

--- src/Day1.kt
+++ src/Day1.kt
@@ -2,21 +2,21 @@ class Day1(fileName: String) {
     private val fileService = FileService(fileName)
     private val lines = fileService.getLinesAsInt()
 
-    fun solvePart1(): String {
+    fun solvePart1(): Int {
         var count = 0
         for (i in 0..lines.size - 2) {
             count += if (lines[i] < lines[i + 1]) 1 else 0
         }
-        return "\tCount: $count"
+        return count
     }
 
-    fun solvePart2(): String {
+    fun solvePart2(): Int {
         var count = 0
         for (i in 0..lines.size-4) {
             val num1 = lines[i] + lines[i+1] + lines[i+2]
             val num2 = lines[i+1] + lines[i+2] + lines[i+3]
             count += if (num1 < num2) 1 else 0
         }
-        return "\tCount: $count"
+        return count
     }
 }
